865ae63eefd19b0cf523acee5d2f2f27750894f3
Kebin <keninadod@gmail.com>
2024-05-07 19:14:25 +0545

    Closer Function

--- type.js
+++ type.js
@@ -1,7 +1,25 @@
-const foo =(a,b=0) => {
+//const foo =(a,b=0) => {
     const doubleValue = (x) =>
         {return (x*2)}
-    return doubleValue(a+b);
-}
+    return doubleValue;
+    
+//}
+//const bar = () =>
+ //   {result()}
 
-console.log(foo(36));
+//console.log(foo(36,45));
+
+const foo = (cb) => 
+    { 
+        console.log("I am in foo")
+        cb()
+    }
+
+const bar = () =>
+    {
+        console.log("I am in bar")
+    }
+foo=() => {
+    //...
+}
+foo(bar)
