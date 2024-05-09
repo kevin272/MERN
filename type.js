02e95dbce07ad631770eda04f32a96ed91f020de
Kebin <keninadod@gmail.com>
2024-05-09 18:25:58 +0545

    README UPDATE

---
+++ type.js
@@ -0,0 +1,11 @@
+//const foo =(a,b=0) => {
+    const doubleValue = (x) =>
+        {return (x*2)}
+    return doubleValue;
+    
+//}
+//const bar = () =>
+ //   {result()}
+
+//console.log(foo(36,45));
+
