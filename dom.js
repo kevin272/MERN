489ea11e66f7b4663303aab08dfb29e41f3fe785
Kebin <keninadod@gmail.com>
2024-05-10 19:05:47 +0545

    Day 7

--- dom.js
+++ dom.js
@@ -1,2 +1,20 @@
 console.log("Hello World")
 document.write("<h1>Hello World</h1>")
+//debugger;
+document.getElementById('first').innerHTML= 'Update value'
+document.querySelector('#first').innerHTML='Query Selector'
+
+//TRL
+//setTimeout(()=>{console.log("one time caller")},1000);
+//const interval = setInterval(()=>{console.log("inside interval")},1000);
+//clearInterval(interval);
+//clearTimeout();
+setInterval(()=>{
+    const today= new Date();
+    const hour = today.getHours();
+    const minute = today.getMinutes();
+    const second = today.getSeconds();
+
+    document.getElementById('clock').innerHTML= hour+":"+minute+":"+second
+
+},1000)
