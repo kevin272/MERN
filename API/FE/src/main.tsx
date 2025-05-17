b4b570ed21b07df015570c6079797f1dacbba9ec
Kebin <keninadod@gmail.com>
2025-05-17 07:45:57 +0545

    change folder structure and start frontend

---
+++ API/FE/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Landingpage from './pages/landing'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Landingpage />
+  </StrictMode>,
+)
---
+++ API/FE/src/pages/landing/index.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div>
+        <h1>Landing Page</h1>
+        <p>Welcome to the landing page!</p>
+        </div>
+    );
+    }
+export default LandingPage;
